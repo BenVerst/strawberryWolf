31ea8faeeaf287b4fff2fabad66bb9a9e79dda78
BenVerst <Bjwversteeg@gmail.com>
2023-08-18 14:27:21 -0700

    Initial commit

---
+++ R/blingo_bongo.R
@@ -0,0 +1,13 @@
+#' Your Blingo Bongos
+#'
+#' @param blingo Yes
+#' @param bongo Absolutely
+#'
+#' @return
+#' @export
+#'
+#' @examples
+blingo_bongo <- function(blingo, bongo) {
+  print(paste("You currently have", blingo, "blingos and", bongo, "bongos :O"))
+}
+#make sure to run devtools::document()
---
+++ R/burritos_cost.R
@@ -0,0 +1,14 @@
+#' Calculate total burrito cost
+#'
+#' @param price Price per burrito in dollars
+#' @param count Number of burritos in order
+#'
+#' @return
+#' @export
+#'
+#' @examples
+burritos_cost <- function(price, count) {
+  price * count
+}
+#make sure to run devtools::document()
+
